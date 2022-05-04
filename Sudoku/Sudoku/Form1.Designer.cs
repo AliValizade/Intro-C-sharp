5a9907bcadcfa310e42d360ad3afde023c218b6c
alivalizade <alivalizade@gmail.com>
2022-05-05 00:33:41 +0430

    my first commit

--- Sudoku/Sudoku/Form1.Designer.cs
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sudoku
 {
-    partial class Form1
+    partial class Sudoku
     {
         /// <summary>
         /// Required designer variable.
@@ -30,6 +30,8 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btn_newgame = new System.Windows.Forms.Button();
+            this.check_btn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -48,41 +50,63 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_newgame, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.check_btn, 7, 9);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 9;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(341, 349);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // btn_newgame
             // 
-            this.btn_newgame.Location = new System.Drawing.Point(31, 380);
+            this.btn_newgame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_newgame.AutoSize = true;
+            this.btn_newgame.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tableLayoutPanel1.SetColumnSpan(this.btn_newgame, 4);
+            this.btn_newgame.Location = new System.Drawing.Point(3, 309);
             this.btn_newgame.Name = "btn_newgame";
-            this.btn_newgame.Size = new System.Drawing.Size(75, 23);
+            this.btn_newgame.Size = new System.Drawing.Size(142, 37);
             this.btn_newgame.TabIndex = 1;
             this.btn_newgame.Text = "New Game";
-            this.btn_newgame.UseVisualStyleBackColor = true;
+            this.btn_newgame.UseVisualStyleBackColor = false;
             this.btn_newgame.Click += new System.EventHandler(this.btn_newgame_Click);
             // 
-            // Form1
+            // check_btn
+            // 
+            this.check_btn.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tableLayoutPanel1.SetColumnSpan(this.check_btn, 2);
+            this.check_btn.Location = new System.Drawing.Point(262, 309);
+            this.check_btn.Name = "check_btn";
+            this.check_btn.Size = new System.Drawing.Size(76, 37);
+            this.check_btn.TabIndex = 2;
+            this.check_btn.Text = "Check";
+            this.check_btn.UseVisualStyleBackColor = false;
+            this.check_btn.Click += new System.EventHandler(this.check_btn_Click);
+            // 
+            // Sudoku
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(363, 415);
-            this.Controls.Add(this.btn_newgame);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Sudoku";
+            this.Text = "Sudoku";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +116,7 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btn_newgame;
+        private System.Windows.Forms.Button check_btn;
     }
 }
 
--- Sudoku/Sudoku/Form1.cs
+++ Sudoku/Sudoku/Form1.cs
@@ -11,25 +11,53 @@ using System.IO;
 
 namespace Sudoku
 {
-    public partial class Form1 : Form
+    public partial class Sudoku : Form
     {
-        public Form1()
+        TextBox[,] cells;
+        int n = 9;
+        public Sudoku()
         {
             InitializeComponent();
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    TextBox t = new TextBox();
-                    t.Multiline = true;
-                    t.TextAlign = HorizontalAlignment.Center;
-                    t.Font = new Font("Tahoma", 25);
-                    t.Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Bottom;
-                    tableLayoutPanel1.Controls.Add(t, i, j);
+            cells = new TextBox[n, n];
 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    cells[i, j] = new TextBox();
+                    cells[i, j].Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Bottom;
+                    tableLayoutPanel1.Controls.Add(cells[i, j], i, j);
+                    cells[i, j].MaxLength = 1;
+                    cells[i, j].TextAlign = HorizontalAlignment.Center;
+                    cells[i, j].Font = new Font("Tahoma", 14);
+                    cells[i, j].BackColor = Color.LightBlue;
+                    cells[i, j].Click += new EventHandler(textbox_click);
                 }
             }
         }
+        
+
+        private void textbox_click(object sender, EventArgs e)
+        {
+            //----------------------The User can only Enter a number ------------------------
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < n; j++)
+                {
+                    if (cells[i, j].Text == "1" || cells[i, j].Text == "2" || cells[i, j].Text == "3" || cells[i, j].Text == "4" || cells[i, j].Text == "5" || cells[i, j].Text == "6" || cells[i, j].Text == "7" || cells[i, j].Text == "8" || cells[i, j].Text == "9")
+                    {
+                        //Nothing
+                    }
+                    else
+                    {
+                        cells[i, j].Text = "";
+                    }
+                }
+            }
+        }
+
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -38,23 +66,109 @@ namespace Sudoku
 
         private void btn_newgame_Click(object sender, EventArgs e)
         {
-            OpenFileDialog x = new OpenFileDialog();
-
-            if(x.ShowDialog() == DialogResult.OK)
+            //------------------------Empty sudoku table-------------------
+            for (int i = 0; i < n; i++)
             {
-                // MessageBox.Show("Yes");
+                for (int j = 0; j < n; j++)
+                {
+                    cells[i, j].ReadOnly = false;
+                    cells[i, j].Text = "";
+                    cells[i, j].BackColor = Color.LightBlue;
+                }
+            }
+            //--------------------------------End--------------------------
+
+            btn_newgame.BackColor = Color.LightBlue;
+            OpenFileDialog x = new OpenFileDialog();
+            if (x.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show("Yes");
                 string file_path = x.FileName;
                 //MessageBox.Show(file_path);
                 StreamReader my_file = new StreamReader(file_path);
-
                 string big_text = my_file.ReadToEnd();
-                //MessageBox.Show(s);
-
-                char[] my_seperators = { ' ', '\n' };
-                string[] numbers = big_text.Split(my_seperators); // size 81
-                
-            }
-            
+                //MessageBox.Show(big_text);
+                big_text = big_text.Replace("\n", "");
+                string[] numbers = big_text.Split(); // size 81
+                int index = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (numbers[index] != "0")
+                        {
+                            cells[j, i].ReadOnly = true;
+                            cells[j, i].Text = numbers[index];
+                            cells[j, i].BackColor = Color.LightGreen;
+                        }
+                        index++;
+                    }
+                }
+            }              
         }
+
+       
+        private void check_btn_Click(object sender, EventArgs e)
+        {
+            // --------------------- row & col check -----------------------
+            int x, y, count = 0;  
+            for (int i = 0; i < n; i++)
+            {
+                int[] row = new int[n];
+                int[] col = new int[n];
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (cells[i, j].Text != "" && cells[j, i].Text != "")
+                    {
+                        x = Convert.ToInt32(cells[i, j].Text);
+                        y = Convert.ToInt32(cells[j, i].Text);
+                        if (x >= 1 && x <= n && !row.Contains(x))
+                        {
+                            row[j] = x;
+                        }
+                        if (y >=0 && y <= n && !col.Contains(y))
+                        {
+                            col[j] = y;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("You must be filled all of textboxes!");
+                        return;
+                    }
+                    
+                }
+            }
+
+            // -------------------------- squre check ------------------------
+            for (int k = 0; k < n-2; k+=3)
+            {
+                int[] sq = new int[n];
+                for (int l = 0; l < n-2; l+=3)
+                {
+                    for (int m = k; m < k+3; m++)
+                    {
+                        for (int p = l; p < l+3; p++)
+                        {
+                            if (cells[m, p].Text != "" && cells[p, m].Text != "")
+                            {
+                                x = Convert.ToInt32(cells[m, p].Text);
+                                if (x >= 1 && x <= n && !sq.Contains(x))
+                                {
+                                    sq[count] = x;
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Solved Ok. ❤✔");
+
+        }
+        
+
     }
 }
